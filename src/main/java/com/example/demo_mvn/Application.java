bfb8513d51e1ec3f459394640c69d83079cd19ae
Prabhu <rajendranprabhu.r@gmail.com>
2025-03-18 22:41:35 -0700

    adding spring retry dependency

--- src/main/java/com/example/demo_mvn/Application.java
+++ src/main/java/com/example/demo_mvn/Application.java
@@ -4,18 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @EnableScheduling
+@EnableRetry
 @EnableAsync
 @SpringBootApplication
-public class DemoMvnApplication {
+public class Application {
 
     public static void main(String[] args) {
         log.info("Application starting.....");
-        SpringApplication.run(DemoMvnApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
